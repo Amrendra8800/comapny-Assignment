ff084ccd0e8a0f04b0b17f68529da865ab756993
AMRENDRA KUMAR SINGH <66840911+Amrendra8800@users.noreply.github.com>
2023-08-01 01:15:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-const express=require('express')
+const http = require('http');
 
-const app=express();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
 
-app.use(express.urlencoded({extended:true}))
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello, World')
-})
-
-const PORT=3000
-app.listen(PORT,(req,res)=>{
- console.log(`server is listening on port ${PORT}`)
-})
-
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
 //for this file to execute
 //node server.js and goto the browser and type "localhost:3000" and hit the Enter button
